02532f024123dd38e62b2599d81f1140748bed9e
tomg55 <141691194+tomg55@users.noreply.github.com>
2023-09-15 08:49:29 -0400

    Chore: Updated Code to work with Firmware 1.0.3

--- Relay.js
+++ Relay.js
@@ -1,5 +1,7 @@
- let CONFIG = {
-  input: null,
+//Firmware 1.0.3
+
+let CONFIG = {
+  input: true,
   timeMs: 500,
   state: true
 };
@@ -7,11 +9,15 @@ let deviceInfo=Shelly.getDeviceInfo();
 let deviceid=deviceInfo.id;
 let uptimeTopic = deviceid+"/uptime";
 let switchOn = deviceid+"/switchOn";
-
-
+let inputstate= deviceid+ "/status/input:0/state";
+MQTT.publish(inputstate, JSON.stringify(!CONFIG.input),0,true);
 print (deviceid);
 function callback(userdata) {
-   Shelly.call("Input.GetStatus",{id:0},function (val) {CONFIG.input=(val.state);});
+    Shelly.call("Input.GetStatus",{id:0},function (val) {
+    if (val.state=! null){
+     CONFIG.input=(val.state)
+     MQTT.publish(inputstate, JSON.stringify(CONFIG.input),0,true);
+    }});
    if (CONFIG.input){ 
       Shelly.call("Switch.Set",{ id: 0, on: CONFIG.state });
     }
@@ -26,17 +32,17 @@ function callback(userdata) {
 
 
 function mqtton(topic, message, userdata){
-   let value=JSON.parse(message);
-   if (value ===1){
-   CONFIG.state=true;}
-   else{
-   CONFIG.state=false;}
-   print(JSON.parse(message));
+   try {
+     let value=JSON.parse(message);
+     if (value ===1){
+     CONFIG.state=true;}
+     else{
+     CONFIG.state=false;}
+     print(JSON.parse(message));
+   } catch (error) {
+    // Handle the JSON parsing error here
+    print("Error parsing JSON for SwitchOn: " + error.message);
   }
-
-
-
+  }
 MQTT.subscribe(switchOn, mqtton);
-
-
 let time_handle=Timer.set(CONFIG.timeMs,true,callback,null);
