880636a36a39c5b242320db2992496e7e9fd47a3
tomg55 <141691194+tomg55@users.noreply.github.com>
2023-09-15 08:04:40 -0400

    Update Thermostat.js

--- Thermostat.js
+++ Thermostat.js
@@ -1,7 +1,7 @@
 let CONFIG = {
   deadband: 0.1,
-  setpoint: 99.5,
-  timeMs:500,
+  setpoint: 70,
+  timeMs:1000,
   sensor: 100,
   sensorMax: 102,
   sensorMin: 100,
@@ -12,28 +12,22 @@ let CONFIG = {
 
 let deviceInfo=Shelly.getDeviceInfo();
 let deviceid=deviceInfo.id;
-print ("Device ID: "deviceid);
+print ("Device ID: " + deviceid);
 let setpointTopic = deviceid+"/TempSetpoint";
 let deadbandTopic = deviceid+"/TempDeadband";
 let sensorTopic = deviceid+"/TempSensor";
 let thermostatTopic = deviceid+"/ThermostatControl";
-let inputid= deviceid+ "/status/input:0/id";
-let inputstate= deviceid+ "/status/input:0/state";
 let uptimeTopic=deviceid+"/uptime";
 MQTT.publish(setpointTopic, JSON.stringify(CONFIG.setpoint),0,true);
 MQTT.publish(deadbandTopic, JSON.stringify(CONFIG.deadband),0,true);
 MQTT.publish(sensorTopic, JSON.stringify(CONFIG.sensor),0,true);
 MQTT.publish(thermostatTopic, JSON.stringify(CONFIG.thermostat),0,true);
-MQTT.publish(inputstate, JSON.stringify(!CONFIG.input),0,true);
+
 function callback(userdata) {
   Shelly.call("temperature.getStatus",{ id: CONFIG.sensor },function (response) {
     let temp=(response.tF);
     Shelly.call("Input.GetStatus",{id:0},function (val) {
-    if (val.state=! null){
-     CONFIG.input=(val.state)
-     MQTT.publish(inputstate, JSON.stringify(CONFIG.input),0,true);
-    }});
-    
+    CONFIG.input=(val.state);});
     if (MQTT.isConnected()){
       if (CONFIG.thermostat && CONFIG.input){
         if (temp > CONFIG.setpoint || temp>CONFIG.maxTemp){
@@ -58,31 +52,52 @@ null
    
 }
 
-function mqttsetpoint(topic, message, userdata){
-CONFIG.setpoint=JSON.parse(message);
-print ("Setpoint: "+message);
+function mqttsetpoint(topic, message, userdata) {
+  try {
+    CONFIG.setpoint = JSON.parse(message);
+    print("Setpoint: " + message);
+  } catch (error) {
+    // Handle the JSON parsing error here
+    print("Error parsing JSON for setpoint: " + error.message);
+  }
 }
 
-function mqttdeadband(topic, message, userdata){
-CONFIG.deadband=JSON.parse(message);
-print ("Deadband: "+message);
+function mqttdeadband(topic, message, userdata) {
+  try {
+    CONFIG.deadband = JSON.parse(message);
+    print("Deadband: " + message);
+  } catch (error) {
+    // Handle the JSON parsing error here
+    print("Error parsing JSON for deadband: " + error.message);
+  }
 }
 
 function mqttsensor(topic, message, userdata){
-let sensor=JSON.parse(message);
-if (sensor>CONFIG.sensorMax){
-CONFIG.sensor=CONFIG.sensorMax;}
-else if (sensor<CONFIG.sensorMin){
-CONFIG.sensor=CONFIG.sensorMin;}
-else{
-CONFIG.sensor=sensor;}
-print ("Sensor: "+message);
+   try {
+    let sensor=JSON.parse(message);
+    if (sensor>CONFIG.sensorMax){
+    CONFIG.sensor=CONFIG.sensorMax;}
+    else if (sensor<CONFIG.sensorMin){
+    CONFIG.sensor=CONFIG.sensorMin;}
+    else{
+    CONFIG.sensor=sensor;}
+    print ("Sensor: "+message);
+   } catch (error) {
+      // Handle the JSON parsing error here
+    print("Error parsing JSON for thermostat: " + error.message);
+  }
 }
 
 function mqttthermostat(topic, message, userdata){
-CONFIG.thermostat=JSON.parse(message);
-print ("Thermostat: "+message);
+  try {
+    CONFIG.thermostat=JSON.parse(message);
+    print ("Thermostat: "+message);
+  } catch (error) {
+    // Handle the JSON parsing error here
+    print("Error parsing JSON for thermostat: " + error.message);
+  }
 }
+
 
 
 
